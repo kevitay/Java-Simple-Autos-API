b8c9c9ca1bbf3996df75afde064d59766178a2e9
Kevin Taylor <kevin.taylor.d8dn@statefarm.com>
2023-04-27 10:53:07 -0500

    GREEN: addAutoValidReturnsAuto test passes

--- src/test/java/com/galvanize/autos/AutosServiceTest.java
+++ src/test/java/com/galvanize/autos/AutosServiceTest.java
@@ -11,6 +11,7 @@ import java.util.Arrays;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.when;
 
@@ -48,11 +49,14 @@ class AutosServiceTest {
     }
 
     @Test
-    void testGetAutos1() {
-    }
-
-    @Test
-    void addAuto() {
+    void addAutoValidReturnsAuto() {
+        Automobile automobile = new Automobile(1967, "Ford", "Mustang", "AABBCC");
+        automobile.setColor("Red");
+        when(autosRepository.save(any(Automobile.class)))
+                .thenReturn(automobile);
+        Automobile auto = autosService.addAuto(automobile);
+        assertThat(auto).isNotNull();
+        assertThat(auto.getMake()).isEqualTo("Ford");
     }
 
     @Test
